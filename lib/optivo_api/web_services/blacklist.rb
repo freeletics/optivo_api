1f57d830eb87e929f1e17a4c82fd4ecb8e9a94ad
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2019-04-25 13:37:49 +0200

    Add blacklist quering

--- lib/optivo_api/web_services/blacklist.rb
+++ lib/optivo_api/web_services/blacklist.rb
@@ -16,5 +16,12 @@ module OptivoApi::WebServices
     def remove(email:)
       fetch_value(:remove, entry: email)
     end
+
+    def all_chuncked(start: 0, size: 1_000)
+      results = fetch_value(:get_all_advanced_flat, pageStart: start, pageSize: size)[:get_all_advanced_flat_return]
+      results.each_slice(5).map do |slice|
+        {email: slice[2], reason: slice[3], blacklisted_at: slice[4]}
+      end
+    end
   end
 end
